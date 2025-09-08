d91917b6046b1f5c4d5bb5ff84e5dc4e9f07d786
student <lwambsrat@gmail.com>
2025-09-08 22:26:48 +0300

    feat:implement detainee table for the cases app

--- api/views.py
+++ api/views.py
@@ -1,15 +1,18 @@
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import  IsAuthenticated, AllowAny
 from django.shortcuts import render
-from cases.models import CaseAssignment, Case
-from .serializers import CaseAssignmentSerializer, CaseSerializer
+from cases.models import CaseAssignment,Detainee, Case
+from .serializers import CaseAssignmentSerializer,DetaineeSerializer, CaseSerializer
 
 
 class CaseAssignmentViewSet(viewsets.ModelViewSet):
    queryset = CaseAssignment.objects.all()
    serializer_class = CaseAssignmentSerializer
    permission_classes = [AllowAny]  
-
+class DetaineeViewSet(viewsets.ModelViewSet):
+   queryset = Detainee.objects.all()
+   serializer_class = DetaineeSerializer
+   permission_classes = [AllowAny]  
 class CaseViewSet(viewsets.ModelViewSet):
     queryset = Case.objects.all()
     serializer_class = CaseSerializer
