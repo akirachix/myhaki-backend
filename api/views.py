0415327eb030d9485d1c0db18b4b95583017e80b
Fiona Wesonga <fionahwesonga@gmail.com>
2025-09-09 10:23:51 +0300

    Modify views.py file in the api model

--- api/views.py
+++ api/views.py
@@ -20,16 +20,6 @@ class CPDPointViewSet(viewsets.ModelViewSet):
     queryset = CPDPoint.objects.all()
     serializer_class = CPDPointSerializer
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response({"message": "CPD Points API", "data": serializer.data})
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response({"message": "CPD Point Details", "data": serializer.data})
-
 class CaseViewSet(viewsets.ModelViewSet):
     queryset = Case.objects.all()
     serializer_class = CaseSerializer
