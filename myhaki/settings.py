d2e95d7874ad3e9b2f1d5c9661e332c2684b0ac7
Mahder Belete Bekele <mahderbeletebekele@gmail.com>
2025-09-10 15:17:52 +0300

    added the users app for myhaki app

--- myhaki/settings.py
+++ myhaki/settings.py
@@ -86,15 +86,14 @@ load_dotenv()
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),  # fallback to sqlite3 if not set
+        'NAME': os.getenv('DB_NAME', 'db.sqlite3'),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', ''),
     }
 }
-
 AUTH_USER_MODEL = 'users.User'
 
 # DATABASES = {
