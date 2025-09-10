6b09ed639ba9bd559f9106b4895539fe494ee164
Akeza Saloi <akezasaloi@gmail.com>
2025-09-10 10:50:38 +0300

    Integrated translate plus and location iQ APIs

--- myhaki/settings.py
+++ myhaki/settings.py
@@ -16,6 +16,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+TRANSLATE_PLUS_URL = os.getenv("TRANSLATE_PLUS_URL")
+LOCATIONIQ_URL = os.getenv("LOCATIONIQ_URL")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
